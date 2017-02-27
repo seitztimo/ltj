71bc5682519539a772c90f6d827ae0a83b985068
Rikuoja <riku.oja@kapsi.fi>
2017-02-27 12:57:27 +0200

    Adjust foreignkey ondelete to protect data tables

--- nature/models.py
+++ nature/models.py
@@ -10,6 +10,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # These seem to be the standard building blocks of most ltj tables
+
+
 class Category(models.Model):
     id = models.IntegerField(primary_key=True)
     explanation = models.CharField(max_length=50, blank=True, null=True, db_column='selitys')
@@ -17,11 +19,13 @@ class Category(models.Model):
     class Meta:
         abstract = True
 
+
 class CategoryWithSource(Category):
     source = models.CharField(max_length=50, blank=True, null=True, db_column='lahde')
 
     class Meta:
         abstract = True
+
 
 class Type(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -31,6 +35,8 @@ class Type(models.Model):
         abstract = True
 
 # Now, for the actual data
+
+
 class Origin(CategoryWithSource):
 
     class Meta:
@@ -50,8 +56,8 @@ class Value(Category):
 
 
 class ValueObject(models.Model):
-    value_id = models.ForeignKey(Value, models.DO_NOTHING, db_column='arvoid')
-    object_id = models.ForeignKey('Object', models.DO_NOTHING, db_column='kohdeid')
+    value_id = models.ForeignKey(Value, models.CASCADE, db_column='arvoid')
+    object_id = models.ForeignKey('Object', models.CASCADE, db_column='kohdeid')
 
     class Meta:
         managed = False
@@ -69,14 +75,14 @@ class Occurrence(Category):
 class ObservationSeries(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50, blank=True, null=True, db_column='nimi')
-    person_id = models.ForeignKey('Person', models.DO_NOTHING, blank=True, null=True, db_column='hloid')
+    person = models.ForeignKey('Person', models.PROTECT, blank=True, null=True, db_column='hloid', related_name='observation_series')
     description = models.CharField(max_length=255, blank=True, null=True, db_column='kuvaus')
     start_date = models.DateField(blank=True, null=True, db_column='alkupvm')
     end_date = models.DateField(blank=True, null=True, db_column='loppupvm')
     method = models.CharField(max_length=255, blank=True, null=True, db_column='menetelma')
     notes = models.CharField(max_length=255, blank=True, null=True, db_column='huomioitavaa')
     additional_info = models.CharField(max_length=255, blank=True, null=True, db_column='lisatieto')
-    active = models.BooleanField(db_column='voimassa')
+    valid = models.BooleanField(db_column='voimassa')
 
     class Meta:
         managed = False
@@ -108,7 +114,7 @@ class Publication(models.Model):
     place_of_printing = models.CharField(max_length=50, blank=True, null=True, db_column='painopaikka')
     year = models.CharField(max_length=50, blank=True, null=True, db_column='vuosi')
     additional_info = models.CharField(max_length=255, blank=True, null=True, db_column='lisatieto')
-    publication_type = models.ForeignKey('PublicationType', models.DO_NOTHING, db_column='julktyyppiid')
+    publication_type = models.ForeignKey('PublicationType', models.PROTECT, db_column='julktyyppiid', related_name='publications')
     link = models.CharField(max_length=4000, blank=True, null=True, db_column='linkki')
 
     class Meta:
@@ -126,7 +132,7 @@ class PublicationType(Type):
 class Object(models.Model):
     id = models.IntegerField(primary_key=True)
     type = models.CharField(max_length=10, blank=True, null=True, db_column='tunnus')
-    object_class = models.ForeignKey('Class', models.DO_NOTHING, db_column='luokkatunnus')
+    object_class = models.ForeignKey('Class', models.PROTECT, db_column='luokkatunnus', related_name='objects')
     geometry1 = models.GeometryField()
     name = models.CharField(max_length=80, blank=True, null=True, db_column='nimi')
     description = models.CharField(max_length=255, blank=True, null=True, db_column='kuvaus')
@@ -135,12 +141,12 @@ class Object(models.Model):
     created_time = models.DateField(blank=True, null=True, db_column='digipvm')
     number = models.IntegerField(blank=True, null=True, db_column='numero')
     created_by = models.CharField(max_length=50, blank=True, null=True, db_column='digitoija')
-    protection_level = models.ForeignKey('ProtectionLevel', models.DO_NOTHING, db_column='suojaustasoid')
+    protection_level = models.ForeignKey('ProtectionLevel', models.PROTECT, db_column='suojaustasoid', related_name='objects')
     last_modified_time = models.DateTimeField(blank=True, null=True, db_column='pvm_editoitu')
     last_modified_by = models.CharField(max_length=10, blank=True, null=True, db_column='muokkaaja')
     area = models.FloatField(blank=True, null=True, db_column='pinta_ala')
     text = models.CharField(max_length=4000, blank=True, null=True, db_column='teksti')
-    link = models.CharField(max_length=4000, blank=True, null=True, db_column='teksti_www')
+    text_www = models.CharField(max_length=4000, blank=True, null=True, db_column='teksti_www')
     values = models.ManyToManyField(Value, through=ValueObject, related_name='objects')
     publications = models.ManyToManyField(Publication, through='ObjectPublication', related_name='objects')
 
@@ -151,7 +157,7 @@ class Object(models.Model):
 
 class HistoricalObject(Object):
     archived_time = models.DateTimeField(db_column='historia_pvm')
-    object = models.ForeignKey(Object, models.DO_NOTHING, db_column='kohde_id')
+    object = models.ForeignKey(Object, models.SET_NULL, db_column='kohde_id', blank=True, null=True)
 
     class Meta:
         managed = False
@@ -159,8 +165,8 @@ class HistoricalObject(Object):
 
 
 class ObjectPublication(models.Model):
-    object_id = models.ForeignKey(Object, models.DO_NOTHING, db_column='kohdeid')
-    publication_id = models.ForeignKey(Publication, models.DO_NOTHING, db_column='julkid')
+    object_id = models.ForeignKey(Object, models.CASCADE, db_column='kohdeid')
+    publication_id = models.ForeignKey(Publication, models.CASCADE, db_column='julkid')
 
     class Meta:
         managed = False
@@ -170,13 +176,13 @@ class ObjectPublication(models.Model):
 
 class ObjectLink(models.Model):
     id = models.IntegerField(primary_key=True)
-    object = models.ForeignKey(Object, models.DO_NOTHING, db_column='tekstiid')
+    object = models.ForeignKey(Object, models.CASCADE, db_column='tekstiid', related_name='link')
     link = models.CharField(max_length=4000, blank=True, null=True, db_column='link')
     text = models.CharField(max_length=4000, blank=True, null=True, db_column='linkkiteksti')
-    type = models.ForeignKey('LinkType', models.DO_NOTHING, db_column='tyyppiid')
+    type = models.ForeignKey('LinkType', models.PROTECT, db_column='tyyppiid')
     ordering = models.IntegerField(blank=True, null=True, db_column='jarjestys')
     link_text = models.CharField(max_length=1000, blank=True, null=True, db_column='linkin_teksti ')
-    protection_level = models.ForeignKey('ProtectionLevel', models.DO_NOTHING, db_column='suojaustasoid')
+    protection_level = models.ForeignKey('ProtectionLevel', models.PROTECT, db_column='suojaustasoid')
 
     class Meta:
         managed = False
@@ -184,8 +190,8 @@ class ObjectLink(models.Model):
 
 
 class SpeciesRegulation(models.Model):
-    species_id = models.ForeignKey('Species', models.DO_NOTHING, db_column='lajid')
-    regulation_id = models.ForeignKey('Regulation', models.DO_NOTHING, db_column='saaid')
+    species_id = models.ForeignKey('Species', models.CASCADE, db_column='lajid')
+    regulation_id = models.ForeignKey('Regulation', models.CASCADE, db_column='saaid')
 
     class Meta:
         managed = False
@@ -195,21 +201,21 @@ class SpeciesRegulation(models.Model):
 
 class Observation(models.Model):
     id = models.IntegerField(primary_key=True)
-    location = models.ForeignKey(Object, models.DO_NOTHING, db_column='kohdeid')
-    species = models.ForeignKey('Species', models.DO_NOTHING, db_column='lajid')
-    series = models.ForeignKey(ObservationSeries, models.DO_NOTHING, db_column='hsaid', blank=True, null=True)
-    abundance = models.ForeignKey('Abundance', models.DO_NOTHING, db_column='runsausid', blank=True, null=True)
-    incidence = models.ForeignKey('Incidence', models.DO_NOTHING, db_column='yleisyysid', blank=True, null=True)
-    observer = models.ForeignKey(Person, models.DO_NOTHING, db_column='hloid', blank=True, null=True)
+    location = models.ForeignKey(Object, models.PROTECT, db_column='kohdeid', related_name='observations')
+    species = models.ForeignKey('Species', models.PROTECT, db_column='lajid', related_name='observations')
+    series = models.ForeignKey(ObservationSeries, models.PROTECT, db_column='hsaid', blank=True, null=True, related_name='observations')
+    abundance = models.ForeignKey('Abundance', models.PROTECT, db_column='runsausid', blank=True, null=True, related_name='observations')
+    incidence = models.ForeignKey('Incidence', models.PROTECT, db_column='yleisyysid', blank=True, null=True, related_name='observations')
+    observer = models.ForeignKey(Person, models.PROTECT, db_column='hloid', blank=True, null=True, related_name='observations')
     number = models.CharField(max_length=30, blank=True, null=True, db_column='lkm')
-    mobility = models.ForeignKey('Mobility', models.DO_NOTHING, db_column='liikkumislkid', blank=True, null=True)
-    origin = models.ForeignKey(Origin, models.DO_NOTHING, db_column='alkuperaid', blank=True, null=True)
-    breeding_category = models.ForeignKey('BreedingCategory', models.DO_NOTHING, db_column='pesimisvarmuusid', blank=True, null=True)
+    mobility = models.ForeignKey('Mobility', models.PROTECT, db_column='liikkumislkid', blank=True, null=True, related_name='observations')
+    origin = models.ForeignKey(Origin, models.PROTECT, db_column='alkuperaid', blank=True, null=True, related_name='observations')
+    breeding_category = models.ForeignKey('BreedingCategory', models.PROTECT, db_column='pesimisvarmuusid', blank=True, null=True, related_name='observations')
     description = models.CharField(max_length=255, blank=True, null=True, db_column='kuvaus')
     notes = models.CharField(max_length=100, blank=True, null=True, db_column='huom')
     date = models.DateField(blank=True, null=True, db_column='pvm')
-    occurrence = models.ForeignKey(Occurrence, models.DO_NOTHING, db_column='esiintymaid', blank=True, null=True)
-    protection_level = models.ForeignKey('ProtectionLevel', models.DO_NOTHING, db_column='suojaustasoid')
+    occurrence = models.ForeignKey(Occurrence, models.PROTECT, db_column='esiintymaid', blank=True, null=True)
+    protection_level = models.ForeignKey('ProtectionLevel', models.PROTECT, db_column='suojaustasoid', related_name='observations')
     created_time = models.DateTimeField(db_column='pvm_luotu')
     last_modified_time = models.DateTimeField(blank=True, null=True, db_column='pvm_editoitu')
 
@@ -240,7 +246,7 @@ class Species(models.Model):
     name_sv = models.CharField(max_length=150, blank=True, null=True, db_column='nimi_ruotsi')
     name_en = models.CharField(max_length=150, blank=True, null=True, db_column='nimi_englanti')
     registry_date = models.DateTimeField(blank=True, null=True, db_column='rekisteripvm')
-    protection_level = models.ForeignKey('ProtectionLevel', models.DO_NOTHING, db_column='suojaustasoid')
+    protection_level = models.ForeignKey('ProtectionLevel', models.PROTECT, db_column='suojaustasoid', related_name='species')
     additional_info = models.CharField(max_length=255, blank=True, null=True, db_column='lisatieto')
     code = models.CharField(max_length=20, blank=True, null=True, db_column='koodi')
     link = models.CharField(max_length=4000, blank=True, null=True, db_column='linkki')
@@ -267,8 +273,8 @@ class LinkType(Type):
 
 
 class HabitatTypeRegulation(models.Model):
-    habitat_type_id = models.ForeignKey('HabitatType', models.DO_NOTHING, db_column='ltyyppiid')
-    regulation_id = models.ForeignKey('Regulation', models.DO_NOTHING, db_column='saadosid')
+    habitat_type_id = models.ForeignKey('HabitatType', models.CASCADE, db_column='ltyyppiid')
+    regulation_id = models.ForeignKey('Regulation', models.CASCADE, db_column='saadosid')
 
     class Meta:
         managed = False
@@ -278,11 +284,11 @@ class HabitatTypeRegulation(models.Model):
 
 class HabitatTypeObservation(models.Model):
     id = models.IntegerField(primary_key=True)
-    object = models.ForeignKey(Object, models.DO_NOTHING, db_column='kohdeid')
-    habitat_type = models.ForeignKey('HabitatType', models.DO_NOTHING, db_column='ltyypid')
+    object = models.ForeignKey(Object, models.PROTECT, db_column='kohdeid', related_name='habitat_type_observations')
+    habitat_type = models.ForeignKey('HabitatType', models.PROTECT, db_column='ltyypid', related_name='habitat_type_observations')
     group_fraction = models.IntegerField(blank=True, null=True, db_column='osuus_kuviosta')
     additional_info = models.CharField(max_length=255, blank=True, null=True, db_column='lisatieto')
-    observation_series = models.ForeignKey(ObservationSeries, models.DO_NOTHING, db_column='hsaid')
+    observation_series = models.ForeignKey(ObservationSeries, models.PROTECT, db_column='hsaid', related_name='habitat_type_observations')
     created_time = models.DateTimeField(db_column='pvm_luotu')
     last_modified_time = models.DateTimeField(blank=True, null=True, db_column='pvm_editoitu')
 
@@ -309,7 +315,7 @@ class Class(models.Model):
     id = models.CharField(primary_key=True, max_length=10, db_column='tunnus')
     name = models.CharField(max_length=50, blank=True, null=True, db_column='nimi')
     additional_info = models.CharField(max_length=255, blank=True, null=True, db_column='lisatieto')
-    super_class = models.ForeignKey('Class', blank=True, null=True, db_column='paatunnus')
+    super_class = models.ForeignKey('Class', models.PROTECT, blank=True, null=True, db_column='paatunnus', related_name='subclasses')
     reporting = models.BooleanField(db_column='raportointi')
     www = models.BooleanField()
     metadata = models.CharField(max_length=4000, blank=True, null=True)
@@ -336,7 +342,7 @@ class Abundance(CategoryWithSource):
 
 
 class Tile(models.Model):
-    id = models.ForeignKey(Object, models.DO_NOTHING, db_column='id', primary_key=True)
+    id = models.ForeignKey(Object, models.CASCADE, db_column='id', primary_key=True, related_name='tile')
     number = models.CharField(max_length=10, blank=True, null=True, db_column='nro')
     degree_of_determination = models.IntegerField(blank=True, null=True, db_column='selvitysaste')
     additional_info = models.CharField(max_length=255, blank=True, null=True, db_column='lisatieto')
@@ -370,8 +376,8 @@ class ConservationProgramme(Type):
 
 
 class ProtectionCriterion(models.Model):
-    criterion_id = models.ForeignKey('Criterion', models.DO_NOTHING, db_column='perusteid')
-    protection_id = models.ForeignKey('Protection', models.DO_NOTHING, db_column='suoid')
+    criterion_id = models.ForeignKey('Criterion', models.CASCADE, db_column='perusteid')
+    protection_id = models.ForeignKey('Protection', models.CASCADE, db_column='suoid')
 
     class Meta:
         managed = False
@@ -387,7 +393,7 @@ class ProtectionLevel(Category):
 
 
 class Protection(models.Model):
-    id = models.ForeignKey(Object, models.DO_NOTHING, db_column='id', primary_key=True)
+    id = models.ForeignKey(Object, models.CASCADE, db_column='id', primary_key=True, related_name='protection')
     reported_area = models.CharField(max_length=50, blank=True, null=True, db_column='ilmoitettu_pinta_ala')
     land_area = models.CharField(max_length=50, blank=True, null=True, db_column='maapinta_ala')
     water_area = models.CharField(max_length=50, blank=True, null=True, db_column='vesipinta_ala')
@@ -405,8 +411,8 @@ class Protection(models.Model):
 
 
 class ProtectionConservationProgramme(models.Model):
-    protection_id = models.ForeignKey(Protection, models.DO_NOTHING, db_column='suojeluid')
-    conservation_programme_id = models.ForeignKey(ConservationProgramme, models.DO_NOTHING, db_column='sohjelmaid')
+    protection_id = models.ForeignKey(Protection, models.CASCADE, db_column='suojeluid')
+    conservation_programme_id = models.ForeignKey(ConservationProgramme, models.CASCADE, db_column='sohjelmaid')
 
     class Meta:
         managed = False
@@ -426,8 +432,8 @@ class Criterion(models.Model):
 
 
 class EventRegulation(models.Model):
-    event_id = models.ForeignKey('Event', models.DO_NOTHING, db_column='tapid')
-    regulation_id = models.ForeignKey(Regulation, models.DO_NOTHING, db_column='saaid')
+    event_id = models.ForeignKey('Event', models.CASCADE, db_column='tapid')
+    regulation_id = models.ForeignKey(Regulation, models.CASCADE, db_column='saaid')
 
     class Meta:
         managed = False
@@ -439,12 +445,12 @@ class Event(models.Model):
     id = models.IntegerField(primary_key=True)
     register_id = models.CharField(max_length=20, blank=True, null=True, db_column='diaarinro')
     description = models.CharField(max_length=255, blank=True, null=True, db_column='kuvaus')
-    type = models.ForeignKey('EventType', models.DO_NOTHING, db_column='tapahtumatyyppiid')
+    type = models.ForeignKey('EventType', models.PROTECT, db_column='tapahtumatyyppiid')
     last_modified_by = models.CharField(max_length=20, blank=True, null=True, db_column='paivittaja')
     date = models.DateField(blank=True, null=True, db_column='pvm')
-    person = models.ForeignKey(Person, models.DO_NOTHING, db_column='hloid', blank=True, null=True)
+    person = models.ForeignKey(Person, models.PROTECT, db_column='hloid', blank=True, null=True)
     link = models.CharField(max_length=4000, blank=True, null=True, db_column='linkki')
-    protection_level = models.ForeignKey(ProtectionLevel, models.DO_NOTHING, db_column='suojaustasoid')
+    protection_level = models.ForeignKey(ProtectionLevel, models.PROTECT, db_column='suojaustasoid')
     objects = models.ManyToManyField(Object, through='EventObject', related_name='events')
     regulations = models.ManyToManyField(Regulation, throuhg='EventRegulation', related_name='events')
 
@@ -454,8 +460,8 @@ class Event(models.Model):
 
 
 class EventObject(models.Model):
-    object_id = models.ForeignKey(Object, models.DO_NOTHING, db_column='kohdeid')
-    event_id = models.ForeignKey(Event, models.DO_NOTHING, db_column='tapid')
+    object_id = models.ForeignKey(Object, models.CASCADE, db_column='kohdeid')
+    event_id = models.ForeignKey(Event, models.CASCADE, db_column='tapid')
 
     class Meta:
         managed = False
