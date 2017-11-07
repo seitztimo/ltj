d3eb311d701347e9763b4f2293d9eb4eb7f6b378
Mingfeng Yu <mingfeng.yu@anders.fi>
2017-11-07 16:18:39 +0200

    Fix search_fields for FeatureAdmin
    Replace feature_class with feature_class__name as the former doesn't allow text search.

--- nature/admin.py
+++ nature/admin.py
@@ -70,7 +70,7 @@ class FeaturePublicationInline(admin.TabularInline):
 class FeatureAdmin(admin.OSMGeoAdmin):
     readonly_fields = ('created_by', 'created_time', 'last_modified_by', 'last_modified_time')
     list_display = ('id', 'feature_class', 'name', 'active')
-    search_fields = ('feature_class', 'name')
+    search_fields = ('feature_class__name', 'name',)
     list_filter = ('feature_class', 'active')
     form = FeatureForm
     inlines = [ObservationInline, FeatureLinkInline, FeaturePublicationInline]
