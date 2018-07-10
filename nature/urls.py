41ecbfd5fcde7ab882e3f437f862a8b380d23363
Frank Wickström <frank.wickstrom@anders.fi>
2018-07-10 14:02:30 +0300

    Add observation series report

--- nature/urls.py
+++ nature/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'nature'
 urlpatterns = [
     url(r'^feature-report/(?P<pk>\d+)/$', views.FeatureReportView.as_view(), name='feature-report'),
+    url(r'^observationseries-report/(?P<pk>\d+)/$', views.ObservationSeriesView.as_view(),
+        name='observationseries-report'),
     url(r'^species-report/(?P<pk>\d+)/$', views.SpeciesReportView.as_view(), name='species-report'),
     url(r'^observation-report/(?P<pk>\d+)/$', views.ObservationReportView.as_view(), name='observation-report'),
     url(r'^wfs', views.FeatureWFSView.as_view(), name='wfs'),
