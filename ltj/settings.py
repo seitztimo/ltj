d85e68b8eba2d2fab2b82790eb99199d93ab5b78
Oswald <oskari.tiala@haltu.fi>
2018-11-06 09:08:55 +0200

    Remove deprecated middleware

--- ltj/settings.py
+++ ltj/settings.py
@@ -87,13 +87,12 @@ if env('SENTRY_DSN'):
     }
     INSTALLED_APPS.append('raven.contrib.django.raven_compat')
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
