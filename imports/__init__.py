dbede50f2aa50888755490baf217b26382651744
otiala <44601448+otiala@users.noreply.github.com>
2019-02-25 16:46:04 +0200

    Translate app names (#205)

---
+++ imports/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'imports.apps.ImportsConfig'
--- imports/apps.py
+++ imports/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class ImportsConfig(AppConfig):
     name = 'imports'
-    verbose_name = 'shape-imports'
+    verbose_name = _('shape-imports')
