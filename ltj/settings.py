6b1c5fc427f01b4e828a9489fd763480998f6e4f
Mingfeng Yu <mfdevworld@gmail.com>
2020-10-07 11:10:34 +0300

    Add django-helusers integration
    This commit add the django-helusers integration to the project.
    
    https://github.com/City-of-Helsinki/django-helusers
    
    Refs: #237

--- ltj/settings.py
+++ ltj/settings.py
@@ -46,6 +46,13 @@ env = environ.Env(
     SENTRY_ENVIRONMENT=(str, "unconfigured"),
     WFS_SERVER_URL=(str, "http://localhost/"),
     WFS_NAMESPACE=(str, "ltj-dev"),
+    OIDC_AUDIENCE=(str, ""),
+    OIDC_API_SCOPE_PREFIX=(str, ""),
+    OIDC_REQUIRE_API_SCOPE_FOR_AUTHENTICATION=(bool, False),
+    OIDC_ISSUER=(str, ""),
+    SOCIAL_AUTH_TUNNISTAMO_KEY=(str, ""),
+    SOCIAL_AUTH_TUNNISTAMO_SECRET=(str, ""),
+    SOCIAL_AUTH_TUNNISTAMO_OIDC_ENDPOINT=(str, ""),
 )
 
 # .env file, should load only in development environment
@@ -96,19 +103,22 @@ LOGGING = {
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
+    "helusers.apps.HelusersConfig",
+    "helusers.apps.HelusersAdminConfig",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.gis",
+    "social_django",
     "ckeditor",
     "rest_framework",
     "rest_framework_gis",
     "nature",
     "imports",
     "files",
+    "users",
 ]
 
 # Sentry
@@ -186,6 +196,36 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+# Authentication & Authorization
+AUTH_USER_MODEL = "users.User"
+AUTHENTICATION_BACKENDS = (
+    "helusers.tunnistamo_oidc.TunnistamoOIDCAuth",
+    "django.contrib.auth.backends.ModelBackend",
+)
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
+SESSION_SERIALIZER = "django.contrib.sessions.serializers.PickleSerializer"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": ("helusers.oidc.ApiTokenAuthentication",),
+}
+
+OIDC_API_TOKEN_AUTH = {
+    "AUDIENCE": env("OIDC_AUDIENCE"),
+    "API_SCOPE_PREFIX": env("OIDC_API_SCOPE_PREFIX"),
+    "REQUIRE_API_SCOPE_FOR_AUTHENTICATION": env(
+        "OIDC_REQUIRE_API_SCOPE_FOR_AUTHENTICATION"
+    ),
+    "ISSUER": env("OIDC_ISSUER"),
+}
+
+SOCIAL_AUTH_TUNNISTAMO_KEY = env("SOCIAL_AUTH_TUNNISTAMO_KEY")
+SOCIAL_AUTH_TUNNISTAMO_SECRET = env("SOCIAL_AUTH_TUNNISTAMO_SECRET")
+SOCIAL_AUTH_TUNNISTAMO_OIDC_ENDPOINT = env("SOCIAL_AUTH_TUNNISTAMO_OIDC_ENDPOINT")
+
 
 # Custom settings
 
--- ltj/urls.py
+++ ltj/urls.py
@@ -1,30 +1,15 @@
-"""ltj URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
 from nature.api import router
 
 urlpatterns = [
-    url(r"^admin/", admin.site.urls),
-    url(r"v1/", include(router.urls)),
-    url(r"^ltj/", include("nature.urls")),
+    path("helauth/", include("helusers.urls")),
+    path("admin/", admin.site.urls),
+    path("v1/", include(router.urls)),
+    path("ltj/", include("nature.urls")),
 ]
 
 admin.site.site_title = _("Luontotietojärjestelmä site admin")
-admin.site.site_header = _("Luontotietojärjestelmä administration")
 admin.site.site_url = "/admin"
