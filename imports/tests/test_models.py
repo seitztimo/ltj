c3e0f814f5f14b376abfa73dca9a023856c4a578
Mingfeng Yu <mfdevworld@gmail.com>
2020-10-19 11:15:41 +0300

    Fix test__str__
    The tests are run in random order and the test shapefile may
    have already been created when running test__str__. In such
    cases, an suffix will be appended to the filename to avoid
    collision.
    
    no refs

--- imports/tests/test_models.py
+++ imports/tests/test_models.py
@@ -2,16 +2,15 @@ import os
 
 from django.test import TestCase
 
-from nature.tests.utils import make_user
 from .factories import ShapefileImportFactory
 
 
 class TestShapefileImport(TestCase):
     def setUp(self):
-        self.shp_import = ShapefileImportFactory(created_by=make_user())
+        self.shp_import = ShapefileImportFactory()
 
     def tearDown(self):
         os.remove(self.shp_import.shapefiles.path)
 
     def test__str__(self):
-        self.assertEqual(self.shp_import.__str__(), "shapefiles/testshapefiles.zip")
+        self.assertTrue(str(self.shp_import).startswith("shapefiles/testshapefiles"))
